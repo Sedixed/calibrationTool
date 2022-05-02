ddd1e1bec9925a443b0cfa1c198c6069b5638e71
sedixed <sedixed@gmail.com>
2022-05-02 11:51:00 +0200

    hm

--- calibrationTool/src/App.cpp
+++ calibrationTool/src/App.cpp
@@ -15,7 +15,7 @@ class App : public wxApp {
 wxIMPLEMENT_APP(App);
 
 bool App::OnInit() {
-    long style = wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX);
+    long style = (wxEXEC_HIDE_CONSOLE | wxDEFAULT_FRAME_STYLE) & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX);
     AppFrame *frame = new AppFrame("Camera calibration tool", wxPoint(-1, -1), SIZE, style);
     frame->Show(true);
     return true;
